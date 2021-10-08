ae7db0853160fe315ecbe7ab4b7b8fde7f12a092
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-10-08 10:21:43 +0500

    Working with State and Events

--- App.js
+++ App.js
@@ -2,19 +2,47 @@ import React, { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+  };
+
   return (
-    <View style={{ backgroundColor: "tomato", paddingTop: 50 }}>
-      <View>
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
         <TextInput
-          placeholder='Write'
-          style={{ borderColor: "black", borderWidth: 1 }}
+          placeholder='Course Goal'
+          style={styles.input}
+          value={enteredGoal}
+          onChangeText={goalInputHandler}
         />
+        <Button title='Add' onPress={addGoalHandler} />
       </View>
-      <View>
-        <Button title='Home' />
-      </View>
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    borderColor: "black",
+    width: "80%",
+    borderWidth: 1,
+    padding: 10,
+  },
+});
